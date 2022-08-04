52ff27f67bfa20ff0d42374aa358462c28d1ff22
Akula <110611727+ace5123@users.noreply.github.com>
2022-08-04 18:08:05 +0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+NumberVariable = 29
+StringVariable = "Hello!"
+BooleanVariable = True
+SpriteVariable = game.create_sprite(2, 2)
+list2 = ["red", "orange", "yellow"]
---
+++ main.ts
@@ -0,0 +1,5 @@
+let NumberVariable = 29
+let StringVariable = "Hello!"
+let BooleanVariable = true
+let SpriteVariable = game.createSprite(2, 2)
+let list = ["red", "orange", "yellow"]
